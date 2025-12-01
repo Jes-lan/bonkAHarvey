3f22e7ec7b10d0b8ba485ed6d541953b9002d1ea
Jes-lan <soysalahmetfaruk@gmail.com>
2025-12-01 10:23:38 +0300

    Refactor main game scene and script for button layout adjustments and timer integration

--- scripts/maingame.gd
+++ scripts/maingame.gd
@@ -1,9 +1,14 @@
 extends Control
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var pointFlag : bool = false 
+var harvey : Texture2D = preload("res://icon.svg")
+@onready var timer = $timerForBonk
 
+func _ready():
+	timer.start(2)
+
+func _process(delta):
+	print(timer.time_left)
 
 func _on_button_pressed() -> void:
 	Global.gPoints += 1
@@ -39,3 +44,9 @@ func _on_button_8_pressed() -> void:
 
 func _on_button_9_pressed() -> void:
 	Global.gPoints += 1
+
+
+func _on_timer_for_bonk_timeout() -> void:
+	var sprite2d = Sprite2D.new()
+	add_child(sprite2d)
+	sprite2d.set_texture(harvey)
