b431e2dbf0eac519f6f61956557369a2b953963b
Jes-lan <soysalahmetfaruk@gmail.com>
2025-12-01 10:04:08 +0300

    Implement global points tracking and UI updates for game interactions

--- scripts/maingame.gd
+++ scripts/maingame.gd
@@ -1,11 +1,41 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_pressed() -> void:
+	Global.gPoints += 1
+
+
+func _on_button_2_pressed() -> void:
+	Global.gPoints += 1
+
+
+func _on_button_3_pressed() -> void:
+	Global.gPoints += 1
+
+
+func _on_button_4_pressed() -> void:
+	Global.gPoints += 1
+
+
+func _on_button_5_pressed() -> void:
+	Global.gPoints += 1
+
+
+func _on_button_6_pressed() -> void:
+	Global.gPoints += 1
+
+
+func _on_button_7_pressed() -> void:
+	Global.gPoints += 1
+
+
+func _on_button_8_pressed() -> void:
+	Global.gPoints += 1
+
+
+func _on_button_9_pressed() -> void:
+	Global.gPoints += 1
